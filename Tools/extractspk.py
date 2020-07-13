e8c984cffc767df0063c032b4aec277add21bdda
Vitaly Ostrosablin <tmp6154@yandex.ru>
2020-07-14 01:21:07 +0300

    Add a rewritten in Python SPK extractor
    Signed-off-by: Vitaly Ostrosablin <tmp6154@yandex.ru>

---
+++ Tools/extractspk.py
@@ -0,0 +1,57 @@
+import argparse
+import struct
+from pathlib import Path
+
+PATH = 0
+OFFSET = 1
+
+parser = argparse.ArgumentParser(description="Extract spk archive")
+parser.add_argument("archive", type=Path)
+parser.add_argument("dir", type=Path)
+parser.add_argument("--variant", choices=["eng", "chi"], default="eng")
+args = parser.parse_args()
+
+def read_c_string(fo):
+    readbytes = []
+    while True:
+        try:
+            inchar = fo.read(1)
+        except IOError:
+            break
+        if inchar == b'\x00':
+            break
+        readbytes.append(inchar)
+    return b''.join(readbytes).decode('utf-8')
+    
+
+def read_file_headers(fo, english=True):
+    if not english:
+        fo.read(1)
+    path = read_c_string(fo)
+    fo.read((31 if english else 62)-len(path))
+    offset = struct.unpack("<i", arc.read(4))[0]
+    print(f"path: {path}, offset: {offset}")
+    return (path, offset)
+    
+
+# mkdir if not exists
+args.dir.mkdir(parents=True, exist_ok=True)
+# open the archive
+arc = open(args.archive, 'rb')
+file_count = struct.unpack("<h", arc.read(2))[0]
+print(f'{file_count} files in archive')
+assert file_count > 0, "File count should be more that zero!"
+
+headers = []
+for _ in range(file_count):
+    headers.append(read_file_headers(arc, english=(args.variant == "eng")))
+for i in range(len(headers)):
+    tpath = args.dir / headers[i][PATH]
+    dest = open(tpath, "wb")
+    arc.seek(headers[i][1]+1)
+    if i == len(headers) - 1:
+        dest.write(arc.read())
+    else:
+        dest.write(arc.read(headers[i+1][OFFSET]-headers[i][OFFSET]))
+    dest.close()
+    print(f"Extracted {tpath}")
