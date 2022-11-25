5058a5dd0e8acae9e6c63e92003aefd4243612b3
Yoti <realyoti@gmail.com>
2022-11-25 13:32:46 +0300

    Update SIS extractor for S60v1/v2

--- Tools/sis_unpacker.py
+++ Tools/sis_unpacker.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''
-    sis_unpacker.py v0.1 by Yoti
+    sis_unpacker.py v0.1.1 by Yoti
 '''
 
 import os
@@ -22,9 +22,11 @@ def main():
             out_dir = sys.argv[2]
 
         fin_name = sys.argv[1]
+
         with open(fin_name, 'rb') as fin:
             fin_size = len(fin.read()); fin.seek(0)
             print('in: ' + fin_name + ' (' + str(fin_size) + ')', end='')
+
             uids = struct.unpack('<IIII', fin.read(16))
             if uids[1] != 0x10003a12 or uids[2] != 0x10000419:
                 print()
@@ -34,14 +36,16 @@ def main():
             fin.seek(4, os.SEEK_CUR)
             files_count = struct.unpack('<H', fin.read(2))[0]
             print(' [' + str(files_count) + ']')
+
             fin.seek(30, os.SEEK_CUR)
             files_offset = struct.unpack('<I', fin.read(4))[0]
             fin.seek(files_offset, os.SEEK_SET)
 
             for i in range(0, files_count):
                 file_record = struct.unpack('<IIIIIIIII', fin.read(36))
+                fin.seek(12, os.SEEK_CUR)
+
                 if file_record[0] == 0 or file_record[0] == 1:
-                    fin.seek(12, os.SEEK_CUR)
                     tmp = fin.tell()
                     fin.seek(file_record[6], os.SEEK_SET)
                     fout_name = out_dir + os.sep + fin.read(file_record[5]).decode('utf-16')[3:]
@@ -63,9 +67,6 @@ def main():
                         with open(fout_name, 'wb') as fout:
                             fout.write(fout_data_cmp)
                     fin.seek(tmp, os.SEEK_SET)
-                else:
-                    fin.seek(12, os.SEEK_CUR)
-                    pass
 
 if __name__ == "__main__":
     main()
